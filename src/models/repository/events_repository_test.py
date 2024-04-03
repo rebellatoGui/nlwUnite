c200a188aa82d5fa977fe0cb8c4f95b4f201d64e
Gui <guilhermerebellato7@gmail.com>
2024-04-03 04:24:03 -0300

    feat:Implementing events_repository

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -1,13 +1,15 @@
+import pytest
 from src.models.settings.connection import db_connection_handler
 from .events_repository import EventsRepository
 
 db_connection_handler.connect_to_db()
 
+@pytest.mark.skip(reason="Novo registro em banco de dados")
 def test_insert_event():
     event = {
-        "uuid": "meu-uuid",
+        "uuid": "meu-uuid2",
         "title": "meu-title",
-        "slug": "meu-slug-aqui",
+        "slug": "meu-slug-aqui2",
         "maximum_attendees": 20       
     }
     
@@ -16,4 +18,9 @@ def test_insert_event():
     response = events_repository.insert_event(event)
     print(response)
     
+def test_get_event_by_id():
+        event_id = "meu-uuid2"
+        events_repository = EventsRepository()
+        response = events_repository.get_event_by_id(event_id)
+        print(response)            
     
